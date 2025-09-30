784ca45ff7816be808af78d762e149c429931d24
Ehban <ehbanawan2005@gmail.com>
2025-09-30 16:38:18 -0700

    Add files via upload

---
+++ code/mnist_run.py
@@ -0,0 +1,5 @@
+import mnist_loader, my_network
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+net = my_network.Network([784, 70, 10])
+net.SGD(training_data, 30, 10, 3, test_data)
