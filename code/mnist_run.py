f0fcbec397b6b413b525a95c6fa929d575f2c72d
Ehban <ehbanawan2005@gmail.com>
2025-10-02 16:22:00 -0700

    Update mnist_run.py

--- code/mnist_run.py
+++ code/mnist_run.py
@@ -1,5 +1,6 @@
 import mnist_loader, my_network
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-net = my_network.Network([784, 70, 10])
+net = my_network.Network([784, 30, 10])
+
 net.SGD(training_data, 30, 10, 3, test_data)
